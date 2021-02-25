4062a92f4a63e9d680d30902d4b8bbaf62d17104
heeeyueee <heeeyueee@163.com>
2021-02-25 13:35:19 +0800

    done

---
+++ cli.js
@@ -0,0 +1,27 @@
+const program=require("commander")
+const api=require("./index.js")
+program
+  .option('-x, --xxx', 'what x')
+  program
+  .command('add')
+  .description('add a task')
+  .action((...args) => {
+    const words=args[1].args.join("")
+    api.add(words).then(()=>{console.log("添加成功");},(error)=>{console.log(error);})
+  });
+  program
+  .command('clear')
+  .description('clear tasks')
+  .action((...args) => {
+    api.clear().then(()=>{console.log("清除成功");},()=>{console.log("清除失败");})
+  });
+  program
+  .command('show')
+  .description('show all tasks')
+  .action((...args) => {
+    api.showAll()
+  });
+program.parse(process.argv);
+// if(process.argv.length===2){
+//     api.showAll()
+// }
